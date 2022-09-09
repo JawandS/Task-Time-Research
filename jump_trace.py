0cd83b47603ebdc639e19b65936a5b2bd323fa43
Jawand <jawandsingh@gmail.com>
2022-09-09 09:49:09 -0400

    add more information to jump trace

--- jump_trace.py
+++ jump_trace.py
@@ -20,21 +20,28 @@ print("%-18s %-16s %-6s %s" % ("TIME(s)", "TASK", "PID", "CPU"))
 
 # capture previous data
 prev_data = []
+counter = 0
 
 # format output
 while 1:
     try:
-        data = b.trace_fields() # (task, pid, cpu, flags, ts, msg)
-        if prev_data and data[4] - prev_data[4] > 5:
+        (task, pid, cpu, flags, ts, msg) = b.trace_fields() # (task, pid, cpu, flags, ts, msg)
+        if counter % 1000 == 0:
+            printb(b"%-18.9f %-16s %-6d %s" % (ts, task, pid, msg))
+        if prev_data and ts - prev_data[4] > 5:
             # check there's prev data and the time jump is greater than 5
+            print("Time jump start\n\n\n")
             print((prev_data[4], prev_data[0], prev_data[1], prev_data[2]))
-            print((data[4], data[0], data[1], data[2]))
+            print((ts, task, pid, cpu))
+            print("\n\n\nTime jump end")
             # update the previous data
-            prev_data = data
+            prev_data = (task, pid, cpu, flags, ts, msg)
         else:
             # store the previous data
-            prev_data = data
+            prev_data = (task, pid, cpu, flags, ts, msg)
     except ValueError:
+        print("ValueError")
         continue
     except KeyboardInterrupt:
         exit()
+    counter += 1

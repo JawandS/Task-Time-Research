18071910bbd68769661d9df5150254e2884eeb1f
Jawand <jawandsingh@gmail.com>
2022-10-12 16:43:52 -0400

    NOT WORKING - trying to process data

--- Logs/scraper.py
+++ Logs/scraper.py
@@ -1,44 +1,68 @@
-# move all the timelines and time_diffs to a new folder
-import os, shutil, math
-
-def iter_dirs():
-    # find all the directories in the current location
-    dirs = sorted(name for name in os.listdir(".") if os.path.isdir(os.path.join("", name)))
-    # iterate through the files
-    for dir_name in dirs:
-        # create a new directory for the data
-        os.mkdir("Jawand_Data_Archive/" + dir_name)
-        if os.path.isfile(dir_name + "/Raw/timeline.txt"):
-            shutil.move(dir_name + "/Raw/timeline.txt", "Jawand_Data_Archive/" + dir_name + "/timeline.txt")
-        if os.path.isfile(dir_name + "/Raw/time_diffs.txt"):
-            shutil.move(dir_name + "/Raw/time_diffs.txt", "Jawand_Data_Archive/" + dir_name + "/time_diffs.txt")
-
 def trace_logs(log_num):
-    log = "raw_log_" + log_num + ".txt"
-    total_diff = 0 # total difference between timestamps
-    num_diffs = 0 # number of differences
+    # variables
+    log = "log_" + log_num + ".txt"
+
+    # read the file
     with open(log, "r") as f:
-        prev_ts = 0 # previous timestamp
-        # read through the lines
+        data = {}  # store the data
+        # iterate  through the lines and add it to data
         for counter, line in enumerate(f):
+            # skip the first line (header) or any new line
             if counter == 0 or line == "\n":
-                continue # skip the first line or new line
-            data = line.strip().split(" ")
-            if len(data) != 2:
-                continue # skip empty lines or new lines
-            ts = int(data[1])
-            if counter == 1: # continue if it's the first timestamp
-                prev_ts = ts
                 continue
-            if (prev_ts > 0 and ts < 0) or (prev_ts < 0 and ts > 0): # if the sign changes ignore it
+            # split the data
+            line_val = line.strip().split(" ")
+            # if the line is empty skip it
+            if len(line_val) == 0:
                 continue
-            # calculate the difference
-            diff = ts - prev_ts
-            # add the absolute value of the difference to the total
-            total_diff += diff
-            num_diffs += 1
-            prev_ts = ts
-    print("Average difference for log", log_num, (total_diff / num_diffs) / 1e+6) # print average difference in seconds
+            # get the values and add it to data
+            cpu_num = int(line_val[2])
+            if cpu_num not in data:
+                data[cpu_num] = []
+            data[cpu_num].append((line_val[0], int(line_val[1]), int(line_val[1])))  # pname, pid, ts
+
+        # CPU time for each process
+        task_time = {}
+        pid_time = {}
+        # average length for context switch on each cpu
+        cpu_total = {i: 0 for i in range(len(data))}
+        cpu_switches = {i: 0 for i in range(len(data))}
+        # iterate through the data by cpu
+        for cpu_num in data:
+            prev_ts = 0  # previous timestamp
+            for vals in data[cpu_num]:
+                # get the values
+                pname, pid, ts = vals[0], vals[1], vals[2]
+                if not prev_ts:  # continue if it's the first timestamp
+                    prev_ts = ts
+                    continue
+                # calculate the difference
+                diff = ts - prev_ts
+                # add the absolute value of the difference to the total
+                if pname not in task_time:
+                    task_time[pname] = 0
+                task_time[pname] += diff
+                if pid not in pid_time:
+                    pid_time[pid] = 0
+                pid_time[pid] += diff
+                # add the difference to the cpu total
+                cpu_total[cpu_num] += diff
+                cpu_switches[cpu_num] += 1
+
+    # print average length between context switch in seconds for each CPU
+    for cpu_num in range(len(data)):
+        print("CPU " + str(cpu_num) + ": " + str((cpu_total[cpu_num] / cpu_switches[cpu_num]) / 1e+6) + " seconds")
+
+    # save the data to a file
+    with open("processed_" + log_num + ".txt", "w") as f:
+        for cpu_num in range(len(data)):
+            f.write(
+                "CPU " + str(cpu_num) + ": " + str((cpu_total[cpu_num] / cpu_switches[cpu_num]) / 1e+6) + " seconds\n")
+        for task in task_time:
+            f.write(task + " " + str(task_time[task]) + "\n")
+        for pid in pid_time:
+            f.write(str(pid) + " " + str(pid_time[pid]) + "\n")
+
 
 if __name__ == "__main__":
     trace_logs(log_num="1")

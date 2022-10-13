f9ef512dde46e1ee051f1182cd474f768d0e7f5a
Jawand <jawandsingh@gmail.com>
2022-10-13 15:21:25 -0400

    BUG FIX - wasn't reading in timestamp correctly

--- Logs/scraper.py
+++ Logs/scraper.py
@@ -1,7 +1,6 @@
 def trace_logs(log_num):
     # variables
     log = "log_" + log_num + ".txt"
-
     # read the file
     with open(log, "r") as f:
         data = {}  # store the data
@@ -19,8 +18,7 @@ def trace_logs(log_num):
             cpu_num = int(line_val[2])
             if cpu_num not in data:
                 data[cpu_num] = []
-            data[cpu_num].append((line_val[0], int(line_val[1]), int(line_val[1])))  # pname, pid, ts
-
+            data[cpu_num].append((line_val[0], int(line_val[1]), int(line_val[3])))  # pname, pid, ts
         # CPU time for each process
         task_time = {}
         pid_time = {}
@@ -51,17 +49,32 @@ def trace_logs(log_num):
 
     # print average length between context switch in seconds for each CPU
     for cpu_num in range(len(data)):
-        print("CPU " + str(cpu_num) + ": " + str((cpu_total[cpu_num] / cpu_switches[cpu_num]) / 1e+6) + " seconds")
+        print("CPU " + str(cpu_num))
+        print("\tTotal: " + str(cpu_total[cpu_num] / 1e+9) + "s")
+        print("\tAverage: " + str((cpu_total[cpu_num] / cpu_switches[cpu_num]) / 1e+9) + "s")
+    # calculate total time for all processes vs total time for all processors
+    print("Total task time: " + str(sum(task_time.values()) / 1e+9))
+    print("Total pid time: " + str(sum(pid_time.values()) / 1e+9))
+    print("Total cpu time: " + str(sum(cpu_total.values()) / 1e+9))
 
     # save the data to a file
     with open("processed_" + log_num + ".txt", "w") as f:
         for cpu_num in range(len(data)):
-            f.write(
-                "CPU " + str(cpu_num) + ": " + str((cpu_total[cpu_num] / cpu_switches[cpu_num]) / 1e+6) + " seconds\n")
+            # average time spent on CPU
+            f.write("CPU " + str(cpu_num) + "\n")
+            f.write("\tTotal: " + str(cpu_total[cpu_num] / 1e+9) + "s\n")
+            f.write("\tAverage: " + str((cpu_total[cpu_num] / cpu_switches[cpu_num]) / 1e+9) + "s\n")
+        # calculate total time for all processes vs total time for all processors
+        f.write("Total task time: " + str(sum(task_time.values()) / 1e+9) + "\n")
+        f.write("Total pid time: " + str(sum(pid_time.values()) / 1e+9) + "\n")
+        f.write("Total cpu time: " + str(sum(cpu_total.values()) / 1e+9) + "\n\n")
+        # print the time spent on each process
+        total_time_spent = sum(cpu_total.values())
         for task in task_time:
-            f.write(task + " " + str(task_time[task]) + "\n")
+            f.write(task + " " + str(task_time[task] / 1e+9) + " " + str(round((task_time[task] / total_time_spent) * 100, 3)) + "%\n")
+        print("\n")
         for pid in pid_time:
-            f.write(str(pid) + " " + str(pid_time[pid]) + "\n")
+            f.write(str(pid) + " " + str(pid_time[pid] / 1e+9) + "\n")
 
 
 if __name__ == "__main__":

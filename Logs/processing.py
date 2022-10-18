fddc642883affbe8cbfaf02d6fe6962d14446513
Jawand <jawandsingh@gmail.com>
2022-10-18 11:44:46 -0400

    add processing of the logs by CPU as well as total

--- Logs/processing.py
+++ Logs/processing.py
@@ -1,3 +1,22 @@
+# split the log into separate logs for each CPU
+def split_by_CPU(file_name):
+    # split the log into a dictionary per CPU
+    data = {}
+    with open(file_name, "r") as f:
+        for idx, line in enumerate(f):
+            if line == "\n" or idx == 0:
+                continue
+            # get the values
+            vals = line.split()
+            pname, pid, cpu_num, ts = vals[0], vals[1], vals[2], vals[3]
+            pid, cpu_num, ts = int(pid), int(cpu_num), int(ts)
+            # add the values to the dictionary
+            if cpu_num not in data:
+                data[cpu_num] = []
+            data[cpu_num].append((pname, pid, ts))
+    return data
+
+
 def trace_logs(log_num):
     # variables
     log = "log_" + log_num + ".txt"
@@ -74,10 +93,57 @@ def trace_logs(log_num):
         # print the time spent on each process
         total_time_spent = sum(cpu_total.values())
         for task in task_time:
-            f.write(task + " " + str(task_time[task] / 1e+9) + " " + str(round((task_time[task] / total_time_spent) * 100, 3)) + "%\n")
+            f.write(task + " " + str(task_time[task] / 1e+9) + " " + str(
+                round((task_time[task] / total_time_spent) * 100, 3)) + "%\n")
         print("\n")
         for pid in pid_time:
             f.write(str(pid) + " " + str(pid_time[pid] / 1e+9) + "\n")
+
+
+# a function that calculates the percent time each process takes on each CPU
+def process_time_by_CPU(log_num):
+    data = split_by_CPU("log_" + log_num + ".txt")
+    # calculate the time for each process on each CPU
+    process_time = {}
+    # total time for each CPU
+    cpu_total = {i: 0 for i in range(len(data))}
+    # go through each CPU's log
+    for cpu_num in range(len(data)):
+        # calculate the total time for the CPU
+        cpu_total[cpu_num] = data[cpu_num][-1][2] - data[cpu_num][0][2]
+        # previous timestamp
+        prev_ts = 0
+        # go through the CPU's log
+        for idx, vals in enumerate(data[cpu_num]):
+            # get data
+            pname, pid, ts = vals[0], vals[1], vals[2]
+            if idx == 0:
+                prev_ts = ts
+                continue
+            # calculate the time
+            time_diff = ts - prev_ts
+            # add the time to the dictionary
+            if cpu_num not in process_time:
+                process_time[cpu_num] = {}
+            if pname not in process_time[cpu_num]:
+                process_time[cpu_num][pname] = 0
+            process_time[cpu_num][pname] += time_diff
+            # update the previous timestamp
+            prev_ts = ts
+    # calculate the percent time for each process on each CPU
+    process_percent_time = {}
+    for cpu_num in range(len(data)):
+        if cpu_num not in process_percent_time:
+            process_percent_time[cpu_num] = {}
+        for pname in process_time[cpu_num]:
+            process_percent_time[cpu_num][pname] = process_time[cpu_num][pname] / cpu_total[cpu_num]
+
+    with open("by_CPU_" + log_num + ".txt", "w") as f:
+        for cpu_num in range(len(data)):
+            f.write(f"CPU {cpu_num} total time: {cpu_total[cpu_num] / 1e+9}\n")
+            for pname in process_percent_time[cpu_num]:
+                f.write(f"{pname}: {round(process_percent_time[cpu_num][pname] * 100, 3)}%\n")
+            f.write("\n")
 
 
 if __name__ == "__main__":
@@ -86,4 +152,7 @@ if __name__ == "__main__":
         num = sys.argv[1]
     else:
         num = "6"
+    # trace the logs for analysis
     trace_logs(log_num=num)
+    # calculate the percent time each process takes on each CPU
+    process_time_by_CPU(log_num=num)

4132f63ba6d27800e2b5ccdf199fdb87db93a0dd
JawandS <JawandSingh@gmail.com>
2022-11-02 14:25:13 +0000

    add experiment

--- experiment.sh
+++ experiment.sh
@@ -3,5 +3,5 @@ for counter in {1..500}
 do
     sudo python3 model.py quiet >> Data/run_time.txt & sudo bpftrace -e 'tracepoint:sched:sched_switch { printf("%s %lu %d %lu\n", comm, pid, cpu, nsecs); }' >> temp.txt
     sudo python3 model.py quiet >> Data/run_time.txt
-    echo "Run $counter complete"
+    echo $counter
 done

7b61508dbc2f3fa5aae12eb0fd953f098b85d0e7
Jawand <jawandsingh@gmail.com>
2022-11-02 09:58:53 -0400

    change context_sw0tch_trace ot bpftrace

--- experiment.sh
+++ experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for counter in {1..500}
 do
-    sudo python3 model.py quiet >> Data/run_time.txt & sudo python3 context_switch_trace.py >> temp.txt
+    sudo python3 model.py quiet >> Data/run_time.txt & sudo bpftrace -e 'tracepoint:sched:sched_switch { printf("%s %lu %d %lu\n", comm, pid, cpu, nsecs); }' >> temp.txt
     sudo python3 model.py quiet >> Data/run_time.txt
     echo "Run $counter complete"
 done

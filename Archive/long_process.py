99b47d1da922a1a3e593fd68345452121b6e3df7
JawandS <JawandSingh@gmail.com>
2022-11-09 15:15:02 +0000

    fixed processing

--- Archive/long_process.py
+++ Archive/long_process.py
@@ -2,8 +2,9 @@
 def trace_log():
     data = {} # capture first/last timestamp for each CPU
     with open("long_run.txt", "r") as f:
+        print("opened data")
         for idx, line in enumerate(f):
-            if line == "\n": # ignore new lines
+            if line == "\n" or idx == 0: # ignore new lines
                 continue
             # get the values
             vals = line.split()
@@ -17,5 +18,5 @@ def trace_log():
     for cpu_num in data:
         print("CPU", cpu_num, ":", (data[cpu_num][1] - data[cpu_num][0]) / 1e+9, " seconds")
 
-if __name__ == "main":
-    trace_log()
+if __name__ == "__main__":
+  trace_log()

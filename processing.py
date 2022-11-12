4a55bc28d157f4fee9511072fbd91f3c66c52459
Jawand <jawandsingh@gmail.com>
2022-11-11 21:53:58 -0500

    update data processing methods

--- processing.py
+++ processing.py
@@ -17,56 +17,37 @@ def split_by_CPU(file_name):
     return data
 
 
-def trace_logs(log_num):
+def trace_logs(data, log_num):
     # variables
-    log = "Logs/log_" + log_num + ".txt"
-    # read the file
-    with open(log, "r") as f:
-        data = {}  # store the data
-        # iterate  through the lines and add it to data
-        for counter, line in enumerate(f):
-            # skip the first line (header) or any new line
-            if counter == 0 or line == "\n":
-                continue
-            # split the data
-            line_val = line.strip().split(" ")
-            # if the line is empty skip it
-            if len(line_val) == 0:
-                continue
-            # get the values and add it to data
-            cpu_num = int(line_val[2])
-            if cpu_num not in data:
-                data[cpu_num] = []
-            data[cpu_num].append((line_val[0], int(line_val[1]), int(line_val[3])))  # pname, pid, ts
-        # CPU time for each process
-        task_time = {}
-        pid_time = {}
-        # average length for context switch on each cpu
-        cpu_total = {i: 0 for i in range(len(data))}
-        cpu_switches = {i: 0 for i in range(len(data))}
-        # iterate through the data by cpu
-        for cpu_num in data:
-            prev_ts = 0  # previous timestamp
-            for vals in data[cpu_num]:
-                # get the values
-                pname, pid, ts = vals[0], vals[1], vals[2]
-                if not prev_ts:  # continue if it's the first timestamp
-                    prev_ts = ts
-                    continue
-                # calculate the difference
-                diff = ts - prev_ts
-                # add the absolute value of the difference to the total
-                if pname not in task_time:
-                    task_time[pname] = 0
-                task_time[pname] += diff
-                if pid not in pid_time:
-                    pid_time[pid] = 0
-                pid_time[pid] += diff
-                # add the difference to the cpu total
-                cpu_total[cpu_num] += diff
-                cpu_switches[cpu_num] += 1
-                # update the prev timestamp and values
+    # CPU time for each process
+    task_time = {}
+    pid_time = {}
+    # average length for context switch on each cpu
+    cpu_total = {i: 0 for i in range(len(data))}
+    cpu_switches = {i: 0 for i in range(len(data))}
+    # iterate through the data by cpu
+    for cpu_num in data:
+        prev_ts = 0  # previous timestamp
+        for vals in data[cpu_num]:
+            # get the values
+            pname, pid, ts = vals[0], vals[1], vals[2]
+            if not prev_ts:  # continue if it's the first timestamp
                 prev_ts = ts
+                continue
+            # calculate the difference
+            diff = ts - prev_ts
+            # add the absolute value of the difference to the total
+            if pname not in task_time:
+                task_time[pname] = 0
+            task_time[pname] += diff
+            if pid not in pid_time:
+                pid_time[pid] = 0
+            pid_time[pid] += diff
+            # add the difference to the cpu total
+            cpu_total[cpu_num] += diff
+            cpu_switches[cpu_num] += 1
+            # update the prev timestamp and values
+            prev_ts = ts
 
     # print average length between context switch in seconds for each CPU
     for cpu_num in range(len(data)):
@@ -100,8 +81,7 @@ def trace_logs(log_num):
 
 
 # a function that calculates the percent time each process takes on each CPU
-def process_time_by_CPU(log_num):
-    data = split_by_CPU("Logs/log_" + log_num + ".txt")
+def process_time_by_CPU(data, log_num):
     # calculate the time for each process on each CPU
     process_time = {}
     # total time for each CPU
@@ -144,9 +124,8 @@ def process_time_by_CPU(log_num):
                 f.write(f"{pname}: {round(process_percent_time[cpu_num][pname] * 100, 3)}%\n")
             f.write("\n")
 
-def check_diffs(log_num, min_diff):
-    file_name = "Logs/log_" + log_num + ".txt"
-    data = split_by_CPU(file_name)
+
+def check_diffs(data, log_num, min_diff):
     max_difference = 0
     with open("Data/time_diffs_" + log_num + ".txt", "w") as f:
         for cpu_num in range(len(data)):
@@ -176,15 +155,18 @@ def check_diffs(log_num, min_diff):
                 prev_data = [pname, pid, ts]
         f.write("Max difference: " + str(max_difference) + "\n")
 
+
 if __name__ == "__main__":
     import sys
+
     if len(sys.argv) > 1:
         num = sys.argv[1]
     else:
         num = "13"
+    split_log = split_by_CPU("Logs/log_" + num + ".txt")
     # trace the logs for analysis
-    trace_logs(log_num=num)
+    trace_logs(split_log, num)
     # calculate the percent time each process takes on each CPU
-    process_time_by_CPU(log_num=num)
+    process_time_by_CPU(split_log, num)
     # check for difference greater than 0.1
-    check_diffs(log_num=num, min_diff=0.1)
+    check_diffs(split_log, num, 0.1)

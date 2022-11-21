163db0bc6932cb23b93b400f1cd11bd5a064b5ba
JawandS <JawandSingh@gmail.com>
2022-11-21 19:15:34 +0000

    fix fib experiment

--- fib_experiment.sh
+++ fib_experiment.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-for counter in {1...3}
+for counter in 1 2 3 4 5 6 7 8 9
 do
-  echo "$counter"
-  python3 fib.py 1 & python3 fib.py 2 & python3 fib.py 3 & python3 fib.py 4 & python3 fib.py 5
+  python3 fib.py $counter &
 done

39fd30159d29de3f43a244da8f5c03155ee72274
Jawand <jawandsingh@gmail.com>
2022-11-21 10:11:16 -0500

    inc fib depth

--- fib.py
+++ fib.py
@@ -10,7 +10,7 @@ def fib(n):
 if __name__ == "__main__":
     start_time = time.time()
     args = sys.argv
-    n = 30
+    n = 35
     fib(n)
     if len(args) > 1:
         print(args[1], round(time.time() - start_time, 3))

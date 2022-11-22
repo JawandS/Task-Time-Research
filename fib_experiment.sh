ecccacba29e43d2873c55b1250e0ff81960d4e24
JawandS <JawandSingh@gmail.com>
2022-11-22 14:27:51 +0000

    update script for experiment

--- fib_experiment.sh
+++ fib_experiment.sh
@@ -3,3 +3,5 @@ for counter in 1 2 3 4 5 6 7 8 9
 do
   python3 fib.py $counter &
 done
+sudo bpftrace -e 'tracepoint:sched:sched_switch { printf("%s %lu %d %lu\n", comm, pid, cpu, nsecs); }' >> Logs/log_"$1".txt &
+sudo python3 auto_kill.py 9

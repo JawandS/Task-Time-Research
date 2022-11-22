a5cd6f6a13aa08e9499e84efabd63120882b5774
Jawand <jawandsingh@gmail.com>
2022-11-22 11:03:50 -0500

    update autokill

--- fib_experiment/auto_kill.py
+++ fib_experiment/auto_kill.py
@@ -1,20 +1,23 @@
-import time, os, signal
+import os, signal
 
 THREADS = 9
 counter = 0
-while 1:
-    try:
-        if counter >= THREADS:
-            break
-    except KeyboardInterrupt:
-        counter += 1
 
-name = "bpftrace"
-for line in os.popen("ps ax | grep " + name + " | grep -v grep"):
-	fields = line.split()
-	# extracting Process ID from the output
-	pid = fields[0]
-	# terminating process
-	print("killing " + name + " with pid " + pid)
-	# kill process
-	os.kill(int(pid), signal.SIGINT)  # SIGINT is the signal for "Interrupt"
+
+def caught_sig():
+    global counter
+    counter += 1
+    if counter >= THREADS:
+        name = "bpftrace"
+        for line in os.popen("ps ax | grep " + name + " | grep -v grep"):
+            fields = line.split()
+            # extracting Process ID from the output
+            pid = fields[0]
+            # terminating process
+            print("killing " + name + " with pid " + pid)
+            # kill process
+            os.kill(int(pid), signal.SIGINT)  # SIGINT is the signal for "Interrupt"
+
+
+while 1:
+    signal.signal(signal.SIGUSR1, caught_sig)

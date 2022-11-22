5717ba942756bd0839f114ec16b23df27a611043
Jawand <jawandsingh@gmail.com>
2022-11-22 10:08:40 -0500

    update job

--- fib_experiment/job.py
+++ fib_experiment/job.py
@@ -1,4 +1,4 @@
-import time, sys
+import time, sys, os, signal
 
 # fibonacci function using recursion
 def fib(n):
@@ -12,6 +12,13 @@ if __name__ == "__main__":
     args = sys.argv
     n = 35
     fib(n)
+    name = "auto_kill.py"
+    for line in os.popen("ps ax | grep " + name + " | grep -v grep"):
+        fields = line.split()
+        # extracting Process ID from the output
+        pid = fields[0]
+        # kill process
+        os.kill(int(pid), signal.SIGINT)  # SIGINT is the signal for "Interrupt"
     if len(args) > 1:
         print(args[1], round(time.time() - start_time, 3))
     else:

5a46e10daf8a714779d1d989d4ddc2257e6647e6
JawandS <JawandSingh@gmail.com>
2022-11-22 15:30:50 +0000

    change autokill

--- fib_experiment/auto_kill.py
+++ fib_experiment/auto_kill.py
@@ -2,18 +2,19 @@ import time, os, signal
 
 THREADS = 9
 counter = 0
-while counter < THREADS:
+while 1:
     try:
-        pass
+        if counter >= THREADS:
+            break
     except KeyboardInterrupt:
         counter += 1
 
 name = "bpftrace"
 for line in os.popen("ps ax | grep " + name + " | grep -v grep"):
-fields = line.split()
-# extracting Process ID from the output
-pid = fields[0]
-# terminating process
-print("killing " + name + " with pid " + pid)
-# kill process
-os.kill(int(pid), signal.SIGINT)  # SIGINT is the signal for "Interrupt"
+	fields = line.split()
+	# extracting Process ID from the output
+	pid = fields[0]
+	# terminating process
+	print("killing " + name + " with pid " + pid)
+	# kill process
+	os.kill(int(pid), signal.SIGINT)  # SIGINT is the signal for "Interrupt"
